821ac8794153574787e9cfed5eaad3e65cbc7cfb
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2018-01-18 13:35:46 +0530

    [ADD] Singleton pattern added

---
+++ creational/factory/README.md
@@ -0,0 +1,2 @@
+## Reference
+https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
--- creational/factory/factory.ts
+++ creational/factory/factory.ts
@@ -1,5 +1,65 @@
-class Draw {
-    constructor() {
+interface Shape {
+    draw: any
+}
 
+class Rectangle implements Shape {
+    draw() {
+        console.log('rectangle has been drawn');
     }
 }
+
+class Square implements Shape {
+    draw() {
+        console.log('square has been drawn');
+    }
+}
+
+class Triangle implements Shape {
+    draw() {
+        console.log('triangle has been drawn');
+    }
+}
+
+class Noshape implements Shape {
+    draw() {
+        console.log('no shape will be drawn');
+    }
+}
+
+class ShapeFactory {
+    constructor() { }
+
+    getShape(shapes: string): Shape {
+        let shape = shapes.toLowerCase();
+        switch (shape) {
+            case 'rectangle':
+                return new Rectangle();
+            case 'square':
+                return new Square();
+            case 'triangle':
+                return new Triangle();
+            default:
+                return new Noshape();
+        }
+    }
+}
+
+// Demo class using the ShapeFactory
+class demo {
+    private static shapeFactory = new ShapeFactory();
+
+    constructor() { }
+
+    public static drawAllShapes() {
+        let squareShape = this.shapeFactory.getShape('square');
+        squareShape.draw();
+        let rectangleShape = this.shapeFactory.getShape('rectangle');
+        rectangleShape.draw();
+        let triangleShape = this.shapeFactory.getShape('triangle');
+        triangleShape.draw();
+        let noShape = this.shapeFactory.getShape('noShape');
+        noShape.draw();
+    }
+}
+
+demo.drawAllShapes();
